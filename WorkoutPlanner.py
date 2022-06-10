8e8191b0fd19976e4e0ebb45a09ff533af8d06dc
JackAnlauf <jranlauf@uiowa.edu>
2022-06-10 15:48:55 -0500

    more details

--- WorkoutPlanner.py
+++ WorkoutPlanner.py
@@ -85,10 +85,9 @@
 # 
 # =============================================================================
 
+
+
 print("Ready to work!")
-
-
-
 
 
 
@@ -99,10 +98,46 @@ Simpler version?
     xyz?), known as "hitting a plateu". it tracks data, goal, personal record
     
 
-    reps/sets determined by weight (heavy weight, high sets low weight maybe)
 
-
-
+    programs:
+        
+        
+        create user personal database
+            create users own database according to a template?
+        add exercises  
+            program that adds exercise with data to the excel spreadsheet
+            data:
+                PR = personal record x number of reps
+                Goal = goal x number of reps 
+                Recent = most recent weight for reps
+                    this is added here once and updated to be max of 
+                    day 1, day 2 etc
+            
+                
+        add data
+            to add a single bit of data, usually after a workout
+            ex: 
+                bench press
+                day 1 weight
+                day 1 reps
+            remind user that weight could be body weight x reps (or time)
+            because some exercises are bodyweight for time
+                
+        data analysis 
+            analyzes data (for a specific exercise)to find: 
+                best fitted line(growth rate?), projections, 
+                if certain growth rate, recommend recommendation:
+                requies atleast x datapoints
+                
+        recommendation 
+            activated at request, calculates alternative sets,reps,weight for 
+            exercise. It also adds a new exercise that is named "'exercise'alt" 
+            or somehting like that
+            reps/sets determined by old exercise recent data
+            (like take the last three and average it and reduce by 30% and 
+             increase reps )
+            continue to track this alt exercise from then on until indicated
+            otherwise by user
 
 
 
